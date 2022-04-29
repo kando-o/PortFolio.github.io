1e17a222cdc52aa028ed2251d8c7e037c797abff
Jason Kando <kando.jason@live.fr>
2022-04-29 03:47:16 +0200

    Porfolio V2.0.0 OK/ Voir animation card PortFolio/ Sinon OOOK !!! ^.^

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+// upload mettre du local sur un server
+// load télécharger du local vers
+// download get from server / récuperation depuis un serveur
+// create cration d'el
+// init cration des paramètres par défaut.
+// render du virtuel à --> l'affichage
+// Voir animation portfolio
+// Skal sur les cards stacks
+// animation en biais sur les images des cards 
+// VOIR PARALLAX 
+
 /**
  * @description | Animation du loader
  */
-function loader() {
-
-    loadCards();
-    let card = document.querySelectorAll('.card');
+function loader(card) {
+    let cardPortFolio = document.querySelectorAll('.card__portfolio');
+    console.log(cardPortFolio);
 
     const TLLOAD = gsap.timeline();
 
@@ -13,29 +23,29 @@ function loader() {
         // --> containe image qui part de zéro
         // --> bloc-txt qui part de zéro
         // --> h2 qui part de bot -100px et qui remonte grace a y : 0
-        .to('.contain-image', { height: 400, duration: 2, delay: .3, ease: 'power2.out' })
-        .to('.bloc-txt', { height: 'auto', duration: 1.3, ease: 'power2.out' }, '-=.5')
-        .to('.bloc-txt h2', { y: 0, ease: 'power2.out' }, '-=0.6')
+        .to('.contain__image', { height: 400, duration: 2, delay: .3, ease: 'power2.out' })
+        .to('.bloc__txt', { height: 'auto', duration: 1.3, ease: 'power2.out' }, '-=.5')
+        .to('.bloc__txt h2', { y: 0, ease: 'power2.out' }, '-=0.6')
 
         // Animation du rideau
         // --> background-trans rideau noir aller
         // --> add qui change de background à la fin de l'animation rideau
         // --> background-trans rideau noir retour
-        .to('.background-transition', { y: 0, ease: 'power2.out' }, '-=.2')
+        .to('.bg__transition', { y: 0, ease: 'power2.out' }, '-=.2')
         .add(() => {
             document.querySelector('.flip-img1').style.backgroundImage = "url('./ressources/lionInSuitRed.jpg')";
         })
-        .to('.background-transition', { y: '-100%', delay: .2 })
+        .to('.bg__transition', { y: '-100%', delay: .2 })
 
         // Animation de fin 
         // --> opacity 0 disparition du Loader
         // --> Loader en display none
-        .to('.loader-container', { opacity: 0, duration: .8, delay: 0.7 })
+        .to('.loader__container', { opacity: 0, duration: .8, delay: 0.7 })
         .add(() => {
-            document.querySelector('.load-container').style.display = "none";
-        });
+            document.querySelector('.loader__container').style.display = "none";
+        })
 
-    gsap.registerEffect({
+        gsap.registerEffect({
         name : "bounce",
         effect : (targets, config , y, x) => {
             return gsap.to(targets, {duration : config.duration, x : config.x, y : config.y, scale : config.scale, rotation : config.rotation, ease : config.ease, opacity : config.opacity})
@@ -44,15 +54,14 @@ function loader() {
     })
 
     // Animation card partie PortFolio
-    gsap.to(card[0], { x: -25, delay : 8, duration: 1.8, ease : 'bounce', yoyo:true, repeat : 1} )
-    gsap.to(card[1], { x: 25, delay : 8, duration: 1.8, ease : 'bounce', yoyo:true, repeat : 1} )
-    gsap.to(card[2], { x: -25, delay : 9.5, duration: 1.8, ease : 'bounce', yoyo:true, repeat : 1} )
-    gsap.to(card[3], { x: 25, delay : 9.5, duration: 1.8, ease : 'bounce', yoyo:true, repeat : 1} )
+    gsap.to(cardPortFolio[0], { x: -25, delay : 8, duration: 1.8, ease : 'bounce', yoyo:true, repeat : 1} )
+    gsap.to(cardPortFolio[1], { x: 25, delay : 8, duration: 1.8, ease : 'bounce', yoyo:true, repeat : 1} )
+    gsap.to(cardPortFolio[2], { x: -25, delay : 9.5, duration: 1.8, ease : 'bounce', yoyo:true, repeat : 1} )
+    gsap.to(cardPortFolio[3], { x: 25, delay : 9.5, duration: 1.8, ease : 'bounce', yoyo:true, repeat : 1} )
+
 }
-
-
 // Display card PortFlolio
-const loadCards = ()=>{
+(function loadCards() {
     fetch("data.json")
     .then(res=>res.json())
     .then(data=>{
@@ -60,101 +69,100 @@ const loadCards = ()=>{
         render(data)
         return data
     })
-}
+})()
+/** */
+function loadCard(card) {
+    
+    if(!card.id)return'';
+    return `
+    <div class="col-lg-4 col-md-6 col-12 card-wrapper">
+
+        <div class="card card__portfolio mb-4 shadow" id="${card.name}">
+
+            <span class="card__imagePortfolio">
+            <img src="./ressources/${card.image}" alt="${card.alt}" class="card-img-top " style="height: 10.5rem">
+            </span>
+
+                <div class="card-body">
+                    <h4>${card.title}</h4>
+                    <p class="card-text">${card.description}?</p>
+                </div>
+
+                <div class="d-flex justify-content-between m-3">
+                    <div class="btn-group ">
+
+                    <a href="${card.codeSource}" target="blanc" class="text-dark">
+                        <button class="btn btn-sm btn-outline btn-secondary">
+                            Code source
+                        </button>
+                    </a>
+
+                    <a href="${card.viewProjet}" target="blanc" class="text-dark">
+                        <button class="btn btn-sm btn-outline btn-secondary ml-2">
+                            View Projet
+                        </button>
+                    </a>
+                    
+                    <button class="btn btn-sm btn-outline btn-secondary ml-2" onclick="launchSmartPhone('${card.viewProjet}')" id="${card.id}">
+                        Vieuw Projet SmartPhone
+                    </button>            
+                </div>
+            </div>
+        </div>
+    </div>
+    `;   
+};
+
 function render(card) {
-    loadCardsStacks(card.stacks)
+    loadCardStacks(card.stacks)
     displaySmartPhone(card.projets);
     renderCardStacks(card.stacks)
 }
 
-
-function loadCard(card) {
-    
-    if(!card.id)return'';
-    console.log(card.viewProjet);
-    return `<div class="col-lg-4 col-md-6 col-12 card-wrapper">
-                <div class="card mb-4 shadow-sm" id="${card.name}">
-                        <img src="./ressources/${card.image}" alt="${card.alt}" class="card-img-top " style="height: 10.5rem" >
-                        <div class="card-body">
-                            <h4>${card.title}</h4>
-                            <p class="card-text">${card.description}?</p>
-                        </div>
-
-                        <div class="d-flex justify-content-between m-3">
-                            <div class="btn-group ">
-
-                                <a href="${card.codeSource}" target="_blanc" class="text-dark">
-                                    <button class="btn btn-sm btn-outline btn-secondary">
-                                        Code source
-                                    </button>
-                                </a>
-
-                                <a href="${card.viewProject}" target="_blanc" class="text-dark">
-                                    <button class="btn btn-sm btn-outline btn-secondary ml-2">
-                                        View Projet
-                                    </button>
-                                </a>
-
-                                <button class="btn btn-sm btn-outline btn-secondary ml-2 projet_phone">
-                                    View Projet phone
-                                </button>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>`;
-   
-};
-
-function loadCardsStacks(cards) {
+function loadCardStacks(cards) {
     // console.log(cards);
     cards.map((card) => {
-        console.log(card);
+        console.log();
         document.getElementById('container__stack').innerHTML += `
-        <div class=" col-lg-3 col-md-6  d-flex flex-column align-items-center justify-content-center text-center shadow-sm m-4" style="min-height: 25rem;">
-                                <img src="./ressources/${card.image}" alt="" width="140" height="130">
-                                <h2 class="text-uppercase text-center">${card.name}</h2>
-                                <p >
-                                    ${card.description}
-                                </p>
-                                <a href="#" class="btn btn-secondary btn-services mb-2">Voir mes projets</a>
-                        </div>
-        `;
+        <span class="rainbow text-center d-flex m-4 flex-column justify-content-center align-items-center col-md-6 col-lg-3" >
+
+            <div class=" card__stack flex-column align-items-center justify-content-center text-center" style="height: 20rem;">
+                <img src="./ressources/${card.image}" alt="" width="140" height="130">
+                    <h2 class="text-uppercase text-center">${card.name}</h2>
+                        <p>
+                            ${card.description}
+                        </p>
+            </div>
+        </span>
+
+        `
+        ;
     })
-    // if (!card.name)return'';
-  
-    // containerStack.appendChild('')
-
-
-
 }
-// upload
-// creat
-// init
-//
-// Displaycard Stacks
-// load render
 
+// Displaycard Stacks
+function launchSmartPhone(src){
+    console.log('test');
+    let srcIframe = document.getElementById('srcIframe');
+    document.querySelector(".smartphone__wrapper").classList.remove('smart__hide');
+    document.querySelector(".smartphone__wrapper").classList.add('smart__vieuw');
+    srcIframe.src = src
+}
 
 function displaySmartPhone(card){
 
-    const htmlPhone = document.querySelectorAll('.projet_phone');
+    const btnVieuwSmartPhone = document.querySelectorAll('.projet_phone');
+    let arraymap = []
     let btnCloseSmartPhone = document.querySelector('.smartphone__close');
-
-    htmlPhone.forEach((item) => {
-        console.log();
-        item.onclick = () => {
-            console.log('clicke vieuw');
-            document.querySelector(".smartphone__wrapper").classList.remove('smart__hide')
-            document.querySelector(".smartphone__wrapper").classList.add('smart__vieuw')
-        }
-    });
+    
+    card.map(vp => console.log(vp.viewProjet))
     btnCloseSmartPhone.onclick = () => {
-       document.querySelector('.smartphone__wrapper').classList.add("smart__hide")
-    } 
+        console.log("click");
+        document.querySelector(".smartphone__wrapper").classList.add('smart__hide');
+    }
 };
 
-function loadCardStack(elem, elParent = null, elClass, obj) {
+function loadCardsStack(elem, elParent = null, elClass, obj) {
    
     let createEl = document.createElement(elem);
     if (elClass && elClass.length > 0){elClass.split(" ").map(c => createEl.classList.add(c)) };
@@ -182,7 +190,6 @@ function parsElDom(obj, parent = null){
     return createdEl
 }
 
-
 function renderCardStacks(arrayStack){
     // const containerStack = document.getElementById('container__stack');
 
@@ -192,13 +199,10 @@ function renderCardStacks(arrayStack){
         id++;
         arraymap.index = id
         const objStack = {
-
         }
-        
     })
     parsElDom(arrayStack)
 }
-
 
 
 window.onload = loader;
